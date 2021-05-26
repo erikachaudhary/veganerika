a0276cfd7185603a36a5a44b59c3cea4cc778145
Haseeb Chaudhary <haseebchaudharygmail.com>
2021-05-26 16:01:15 +0200

    seconf commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,21 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
+  # skip_before_action :authenticate_user!, only: [:home ]
+  skip_before_action :authenticate_user!, only: %i[home about blog webdevelopment califa contact]
   def home
   end
+
+  def about
+  end
+
+  def blog
+  end
+  
+  def webdevelopment
+  end
+
+  def Califa
+  end
+  
+  def contact
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'about', to: 'pages#about'
+  get 'blog', to: 'pages#blog'
+  get 'webdevelopment', to: 'pages#webdevelopment'
+  get 'Califa', to: 'pages#Califa'
+  get 'contact', to: 'pages#contact'
   root to: 'pages#home'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
